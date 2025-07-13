5531910d23da180e126ea6ba235cee773d83ee6e
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-07-13 12:27:21 -0500

    Format commands files

--- commands/Welcome.js
+++ commands/Welcome.js
@@ -1,71 +1,68 @@
 const { SlashCommandBuilder } = require("discord.js");
 const interaction = require("../interaction");
-const {WelcomeDB} = require("../database");
+const { WelcomeDB } = require("../database");
 
 module.exports = {
     /** @type {import('discord.js').SlashCommandBuilder} */
     data: new SlashCommandBuilder()
-    .setName('welcome')
-    .setDescription('Add, edit, or disable welcome messages')
-    .addSubcommand(subcommand =>
-        subcommand
-        .setName('add')
-        .setDescription('add or edit the welcome channel')
-        .addStringOption(string =>
-            string
-            .setName('message')
-            .setDescription('What will be the welcome message. Use ${user} = user who joined. Use ${guild} = server name')
-            .setRequired(true)
+        .setName('welcome')
+        .setDescription('Add, edit, or disable welcome messages')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('add')
+                .setDescription('add or edit the welcome channel')
+                .addStringOption(string =>
+                    string
+                        .setName('message')
+                        .setDescription('What will be the welcome message. Use ${user} = user who joined. Use ${guild} = server name')
+                        .setRequired(true)
+                )
         )
-    )
-    .addSubcommand(subcommand =>
-        subcommand
-        .setName('disable')
-        .setDescription('Disable welcome messages')
-    ),
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('disable')
+                .setDescription('Disable welcome messages')
+        ),
 
-/** @param {import('discord.js').ChatInputCommandInteraction} interaction */
-async execute(interaction) {
-    if (!interaction.memberPermissions.toArray().includes("ManageChannels")) {
-        interaction.reply({
-            content: 'You are not authorized to do that',
-            ephemeral: true
-        })
-        return;
-    }
-    const guild = interaction.guild.id;
-    const messageText = interaction.options.getString('message');
-    const subcommand = interaction.options.getSubcommand();
-    if (subcommand === 'add') {
-
-    await WelcomeDB.findOneAndUpdate(
-    {guildID: guild}, 
-    {guildID: guild, message: messageText}, 
-    {upsert: true, new:true});
-
-    interaction.reply(`I will now welcome people with the message \`${messageText}\``);
-
-    } else if (subcommand === 'disable') {
-
+    /** @param {import('discord.js').ChatInputCommandInteraction} interaction */
+    async execute(interaction) {
         if (!interaction.memberPermissions.toArray().includes("ManageChannels")) {
-        interaction.reply({
-            content: 'You are not authorized to do that',
-            ephemeral: true
-        })
-        return;
+            interaction.reply({
+                content: 'You are not authorized to do that',
+                ephemeral: true
+            })
+            return;
+        }
+        const guild = interaction.guild.id;
+        const messageText = interaction.options.getString('message');
+        const subcommand = interaction.options.getSubcommand();
+
+        if (subcommand === 'add') {
+            
+            await WelcomeDB.findOneAndUpdate(
+                { guildID: guild },
+                { guildID: guild, message: messageText },
+                { upsert: true, new: true });
+
+            interaction.reply(`I will now welcome people with the message \`${messageText}\``);
+
+        } else if (subcommand === 'disable') {
+
+            if (!interaction.memberPermissions.toArray().includes("ManageChannels")) {
+                interaction.reply({
+                    content: 'You are not authorized to do that',
+                    ephemeral: true
+                })
+                return;
+            }
+
+            await WelcomeDB.findOneAndDelete({
+                guildID: guild
+            })
+
+            interaction.reply("Disabled Welcome Messages");
+        }
+
     }
-
-
-    await WelcomeDB.findOneAndDelete({
-        guildID: guild
-    })
-
-    interaction.reply("Disabled Welcome Messages");
-    }
-    
-    
-
-
-} 
 };
 
--- commands/croissant.js
+++ commands/croissant.js
@@ -1,72 +1,72 @@
 const { SlashCommandBuilder } = require("discord.js");
-const {CroissantEmojiDB, CroissantMessagesDB, creatorDB} = require("../database");
+const { CroissantEmojiDB, CroissantMessagesDB, creatorDB } = require("../database");
 module.exports = {
     data: new SlashCommandBuilder()
-            .setName('croissant')
-            .setDescription('croissant main command')
-            .addSubcommand(subcommand =>
+        .setName('croissant')
+        .setDescription('croissant main command')
+        .addSubcommand(subcommand =>
             subcommand
-            .setName('add')
-            .setDescription('Add an emoji to the database (creator only)')
-            .addStringOption(string =>
-                string
-                .setName('emoji')
-                .setDescription('select emoji to add')
-                .setRequired(true))
-            .addStringOption(string =>
-                string
-                .setName('name')
-                .setDescription('Name of the Emoji ( Will display in users dropdown)')
-                .setRequired(true))
+                .setName('add')
+                .setDescription('Add an emoji to the database (creator only)')
+                .addStringOption(string =>
+                    string
+                        .setName('emoji')
+                        .setDescription('select emoji to add')
+                        .setRequired(true))
+                .addStringOption(string =>
+                    string
+                        .setName('name')
+                        .setDescription('Name of the Emoji ( Will display in users dropdown)')
+                        .setRequired(true))
         )
         .addSubcommand(subcommand =>
             subcommand
-            .setName('delete')
-            .setDescription('delete a emoji from the database')
-            .addStringOption(string => 
-                string
-                .setName('emoji')
-                .setDescription('What emoji to delete out of the database')
-                .setRequired(true)
-            )
+                .setName('delete')
+                .setDescription('delete a emoji from the database')
+                .addStringOption(string =>
+                    string
+                        .setName('emoji')
+                        .setDescription('What emoji to delete out of the database')
+                        .setRequired(true)
+                )
         )
         .addSubcommand(subcommand =>
             subcommand
-            .setName('delete-messages')
-            .setDescription('Deletes all messages in the database from all users that include a specified emoji')
-            .addStringOption(string =>
-                string
-                .setName('emoji')
-                .setDescription("What emoji to delete all messages with ( Might not show up in dropdown)")
-                .setRequired(true)
-            )
+                .setName('delete-messages')
+                .setDescription('Deletes all messages in the database from all users that include a specified emoji')
+                .addStringOption(string =>
+                    string
+                        .setName('emoji')
+                        .setDescription("What emoji to delete all messages with ( Might not show up in dropdown)")
+                        .setRequired(true)
+                )
         )
         .addSubcommand(subcommand =>
             subcommand
-            .setName('leaderboard')
-            .setDescription('Who has send the most croissants')
-            .addStringOption(string =>
-                string
-                .setName('emoji-name')
-                .setDescription('What croissant to pull up the leaderboard for')
-                .setRequired(false)
-            )
+                .setName('leaderboard')
+                .setDescription('Who has send the most croissants')
+                .addStringOption(string =>
+                    string
+                        .setName('emoji-name')
+                        .setDescription('What croissant to pull up the leaderboard for')
+                        .setRequired(false)
+                )
         )
         .addSubcommand(subcommand =>
             subcommand
-        
-         .setName('stats')
-            .setDescription('Shows the amount of croissants you has sent')
-            .addStringOption(string =>
-                string
-                .setName('emoji-name')
-                .setDescription('What emoji do you want to look up')
-                .setRequired(true))
-            .addUserOption(user =>
-                user
-                .setName('member')
-                .setDescription('Specify a user')
-                .setRequired(false))),
+
+                .setName('stats')
+                .setDescription('Shows the amount of croissants you has sent')
+                .addStringOption(string =>
+                    string
+                        .setName('emoji-name')
+                        .setDescription('What emoji do you want to look up')
+                        .setRequired(true))
+                .addUserOption(user =>
+                    user
+                        .setName('member')
+                        .setDescription('Specify a user')
+                        .setRequired(false))),
 
 
     /** @param {import('discord.js').ChatInputCommandInteraction} interaction */
@@ -74,10 +74,10 @@ module.exports = {
         const subcommand = interaction.options.getSubcommand();
         const guild = interaction.guild.id
         const isTrusted = interaction.memberPermissions.toArray().includes("ManageEmojisAndStickers");
-               if (subcommand === 'stats') {
+        if (subcommand === 'stats') {
             const member = interaction.options.getUser('member');
             const emojiText = interaction.options.getString('emoji-name');
-            const emojiDB = (await CroissantEmojiDB.findOne({ name: emojiText, guildID: guild}).lean());
+            const emojiDB = (await CroissantEmojiDB.findOne({ name: emojiText, guildID: guild }).lean());
 
             if (!emojiDB) {
                 interaction.reply({
@@ -86,27 +86,29 @@ module.exports = {
                 });
                 return;
             }
-                const allEmoji = await CroissantMessagesDB.find({name:emojiDB.name, guildID: guild}).sort({count: -1}).lean();
-                const ranking = allEmoji.findIndex(entry => entry.userID === (member == null ? interaction.user.id : member.id));
-                const result = await CroissantMessagesDB.findOne({
+            const allEmoji = await CroissantMessagesDB.find({ name: emojiDB.name, guildID: guild }).sort({ count: -1 }).lean();
+            const ranking = allEmoji.findIndex(entry => entry.userID === (member == null ? interaction.user.id : member.id));
+            const result = await CroissantMessagesDB.findOne({
 
-                     userID: member == null
-                     ? interaction.user.id
-                     : member.id,
-                     name: `${emojiDB.name}`,
-                     guildID: guild
+                userID: member == null
+                    ? interaction.user.id
+                    : member.id,
+                name: `${emojiDB.name}`,
+                guildID: guild
+            });
+
+            if (result) {
+                const date = new Date(result.timestamp).toLocaleDateString();
+                interaction.reply({
+                    content: ` ${member == null
+                        ? interaction.user
+                        : `<@${member.id}>`} has sent ${result.count} ${emojiDB.emoji} since ${date} and is ranked #${ranking + 1} in ${emojiDB.emoji}`
                 });
-                
-                if (result) {
-                    const date = new Date(result.timestamp).toLocaleDateString();
-                    interaction.reply({content: ` ${member == null
-                     ? interaction.user
-                     : `<@${member.id}>`} has sent ${result.count} ${emojiDB.emoji} since ${date} and is ranked #${ranking + 1 } in ${emojiDB.emoji}`});
-                } else {
-                    interaction.reply(` ${member == null
-                     ? interaction.user
-                     : `<@!${member.id}>`} has not sent any ${emojiDB.emoji}`);
-                }
+            } else {
+                interaction.reply(` ${member == null
+                    ? interaction.user
+                    : `<@!${member.id}>`} has not sent any ${emojiDB.emoji}`);
+            }
 
 
 
@@ -137,10 +139,10 @@ module.exports = {
                 return;
             }
 
-            const newEntry = new CroissantEmojiDB({emoji: emojiID, name: emojiText, guildID: guild});
+            const newEntry = new CroissantEmojiDB({ emoji: emojiID, name: emojiText, guildID: guild });
             await newEntry.save();
 
-            interaction.reply({ content: `Successfully saved ${emojiID} with text \`${emojiText}\` to the database!`})
+            interaction.reply({ content: `Successfully saved ${emojiID} with text \`${emojiText}\` to the database!` })
 
         } else if (subcommand === 'delete') {
             if (!isTrusted) {
@@ -153,7 +155,7 @@ module.exports = {
             const emojiName = interaction.options.getString('emoji');
 
             const entry = await CroissantEmojiDB.findOneAndDelete(
-                {emoji: emojiName, guildID: guild}
+                { emoji: emojiName, guildID: guild }
             )
             if (!entry) {
                 interaction.reply({
@@ -195,67 +197,67 @@ module.exports = {
             if (emojiName === null) {
 
 
-                const top10 = await CroissantMessagesDB.find({ guildID: guild}).sort({count: -1}).limit(10).lean();
+                const top10 = await CroissantMessagesDB.find({ guildID: guild }).sort({ count: -1 }).limit(10).lean();
                 let message = `🏆 Top 10 croissanters since 7/9/25\n\n`//INSERT THE DATE THAT THIS CODE IS IMPLEMENTED
-                for (let i = 0; i <  top10.length; i++) {
-                const userID = top10[i].userID;
-                const count = top10[i].count;
-                const type = await CroissantEmojiDB.findOne({ name: top10[i].name, guildID: top10[i].guildID});;
-                if (i === 0) {
-                    message += `<:gold:1393301072122220575> ${i + 1}. <@!${userID}> — ${count} ${type.emoji}\n`;    
-                } else if (i === 1) {
-                    message += `<:silver:1393301087494209556> ${i + 1}. <@!${userID}> — ${count} ${type.emoji}\n`;
-                } else if (i === 2) {
-                    message += `<:bronze:1393301100307939460> ${i + 1}. <@!${userID}> — ${count} ${type.emoji}\n`;
-                } else {
-                    message += `${i + 1}. <@!${userID}> — ${count} ${type.emoji}\n`;    
+                for (let i = 0; i < top10.length; i++) {
+                    const userID = top10[i].userID;
+                    const count = top10[i].count;
+                    const type = await CroissantEmojiDB.findOne({ name: top10[i].name, guildID: top10[i].guildID });;
+                    if (i === 0) {
+                        message += `<:gold:1393301072122220575> ${i + 1}. <@!${userID}> — ${count} ${type.emoji}\n`;
+                    } else if (i === 1) {
+                        message += `<:silver:1393301087494209556> ${i + 1}. <@!${userID}> — ${count} ${type.emoji}\n`;
+                    } else if (i === 2) {
+                        message += `<:bronze:1393301100307939460> ${i + 1}. <@!${userID}> — ${count} ${type.emoji}\n`;
+                    } else {
+                        message += `${i + 1}. <@!${userID}> — ${count} ${type.emoji}\n`;
+                    }
+
                 }
-                
-            }
-            interaction.reply({content: message});
+                interaction.reply({ content: message });
 
             } else {
-            const emojiEntry = await CroissantEmojiDB.findOne({ name: emojiName, guildID: guild });
+                const emojiEntry = await CroissantEmojiDB.findOne({ name: emojiName, guildID: guild });
 
-            if (!emojiEntry) {
-                interaction.reply({
-                    content: 'That is not a valid emoji',
-                    ephemeral: true
-                });
-                return;
-            }
-
-            const emojiname = emojiEntry.name;
-            const sorted = await CroissantMessagesDB
-                .find({ name: emojiname, guildID: guild })
-                .sort({ count: -1 }) 
-                .limit(10)
-                .lean();
-
-
-            if (sorted.length === 0) {
-                return interaction.reply({ content: 'No data found for that emoji.', ephemeral: true });
-            }
-
-            let message = `🏆 Top 10 croissanters for ${emojiName} since 7/9/25:\n\n`;
-
-            for (let i = 0; i < sorted.length; i++) {
-                const userID = sorted[i].userID;
-                const count = sorted[i].count;
-                if (i === 0) {
-                    message += `<:gold:1393301072122220575> ${i + 1}. <@!${userID}> — ${count} ${emojiEntry.emoji}\n`;    
-                } else if (i === 1) {
-                    message += `<:silver:1393301087494209556> ${i + 1}. <@!${userID}> — ${count} ${emojiEntry.emoji}\n`;
-                } else if (i === 2) {
-                    message += `<:bronze:1393301100307939460> ${i + 1}. <@!${userID}> — ${count} ${emojiEntry.emoji}\n`;
-                } else {
-                    message += `${i + 1}. <@!${userID}> — ${count} ${emojiEntry.emoji}\n`;   
+                if (!emojiEntry) {
+                    interaction.reply({
+                        content: 'That is not a valid emoji',
+                        ephemeral: true
+                    });
+                    return;
                 }
+
+                const emojiname = emojiEntry.name;
+                const sorted = await CroissantMessagesDB
+                    .find({ name: emojiname, guildID: guild })
+                    .sort({ count: -1 })
+                    .limit(10)
+                    .lean();
+
+
+                if (sorted.length === 0) {
+                    return interaction.reply({ content: 'No data found for that emoji.', ephemeral: true });
+                }
+
+                let message = `🏆 Top 10 croissanters for ${emojiName} since 7/9/25:\n\n`;
+
+                for (let i = 0; i < sorted.length; i++) {
+                    const userID = sorted[i].userID;
+                    const count = sorted[i].count;
+                    if (i === 0) {
+                        message += `<:gold:1393301072122220575> ${i + 1}. <@!${userID}> — ${count} ${emojiEntry.emoji}\n`;
+                    } else if (i === 1) {
+                        message += `<:silver:1393301087494209556> ${i + 1}. <@!${userID}> — ${count} ${emojiEntry.emoji}\n`;
+                    } else if (i === 2) {
+                        message += `<:bronze:1393301100307939460> ${i + 1}. <@!${userID}> — ${count} ${emojiEntry.emoji}\n`;
+                    } else {
+                        message += `${i + 1}. <@!${userID}> — ${count} ${emojiEntry.emoji}\n`;
+                    }
+                }
+
+                interaction.reply({ content: message });
+
             }
-
-            interaction.reply({ content: message });
-
-        }
         }
     }
 }
