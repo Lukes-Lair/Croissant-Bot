1b99ef7af52ab86ca2e4b663cf34cc7f2a4aa93b
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-07-13 12:25:32 -0500

    Missed a file

--- setcommands.js
+++ setcommands.js
@@ -1,24 +1,24 @@
- const {REST, Routes, body} = require("discord.js");
- const fs = require('fs');
- const path = require('path');
- const config = require("./config.json");
+const { REST, Routes, body } = require("discord.js");
+const fs = require('fs');
+const path = require('path');
+const config = require("./config.json");
 
 
-    const commandsPath = path.join(__dirname, "commands");
-    const allCommands = fs.readdirSync(commandsPath).filter(c => c.endsWith('.js'));
-    const commands = []
-    for (const file of allCommands) {
-        const command = require(`./commands/${file}`);
-        if ('data' in command) {
-            commands.push(command.data.toJSON())
-        }
+const commandsPath = path.join(__dirname, "commands");
+const allCommands = fs.readdirSync(commandsPath).filter(c => c.endsWith('.js'));
+const commands = []
+for (const file of allCommands) {
+    const command = require(`./commands/${file}`);
+    if ('data' in command) {
+        commands.push(command.data.toJSON())
     }
+}
 
- const rest = new REST({ version: '10' }).setToken(config.token);
-    (async () => {
-        try {
-            await rest.put( Routes.applicationCommands(config.id), {body: commands});
-        } catch (error) {
-            console.error(error);
-        }
-    }) ()
+const rest = new REST({ version: '10' }).setToken(config.token);
+(async () => {
+    try {
+        await rest.put(Routes.applicationCommands(config.id), { body: commands });
+    } catch (error) {
+        console.error(error);
+    }
+})()
