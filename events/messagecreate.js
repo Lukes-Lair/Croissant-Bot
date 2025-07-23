870ad582efe3e5e45eb78490fbaa24ba5bcdbdd8
Luke <202459002+Lukes-Lair@users.noreply.github.com>
2025-07-22 19:22:35 -0500

    Added guild searching to the allEmojis var

--- events/messagecreate.js
+++ events/messagecreate.js
@@ -5,7 +5,7 @@ module.exports = {
     /** @param {import("discord.js").Message} message */
     async executeMessage(message, client) {
         if (message.author.bot) return;
-        const allEmojis = (await CroissantEmojiDB.find().lean()).filter(emojis => message.content.includes(emojis.emoji));
+        const allEmojis = (await CroissantEmojiDB.find({ guildID: message.guild.id}).lean()).filter(emojis => message.content.includes(emojis.emoji));
         if (allEmojis.length === 0) return;
             spam.push(message.author.id);
             if (spam.filter(item => item === message.author.id).length >= 3) {
@@ -13,13 +13,12 @@ module.exports = {
             }
             const user = message.author.id;
             for(const emoji of allEmojis){
-                if (emoji.guildID == message.guild.id){
+                    console.log(allEmojis)
                 await CroissantMessagesDB.findOneAndUpdate(
                     {userID: user, name: emoji.name, guildID: message.guild.id},
                     {$inc: { count: message.content.split(emoji.emoji).length -1 }},
                     {upsert: true, new:true, setDefaultsOnInsert: true}
                 );
-                }
             }
         if (message.content.toLowerCase() === "hehe") {
             message.channel.send("HEHEHEHEHEHE");
