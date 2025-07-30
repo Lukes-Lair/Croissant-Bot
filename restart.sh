72ce61a6fb9f08219fee38d3b2c98585da2a4573
Luke <202459002+Lukes-Lair@users.noreply.github.com>
2025-07-30 11:15:23 -0500

    trying to fix restart command

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd .. || exit 1
-git stash ||exit 1
-git pull || exit 1
-npm install || exit 1
-sudo systemctl restart discord || exit 1
+cd ..
+git stash
+git pull
+npm install
+sudo systemctl restart discord
